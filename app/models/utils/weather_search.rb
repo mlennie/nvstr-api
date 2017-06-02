61dcc18abfbd5395fbe89aed0391403ff0418453
Monty Lennie <montylennie@gmail.com>
2017-06-02 01:10:06 +0000

    searh multiple cities and refactor

--- app/models/utils/weather_search.rb
+++ app/models/utils/weather_search.rb
@@ -8,20 +8,24 @@ class WeatherSearch
   def self.update_city city, results
     current = results["main"]["temp"]
     city[:current] = current
-    city[:id] = results["id"]
+    city[:external_id] = results["id"]
     city[:in_range] = current >= city[:min] && current <= city[:max]
     return city
   end
 
-  def self.search_single_city city
+  def self.update_cities cities, results
+    i = -1
+    cities.map { |c| self.update_city(c, results["list"][i+=1]) }
+  end
+
+  def self.make_request url, city_or_cities, single=false
     error_msg = "There was a problem fetching weather: "
 
     begin
-      city_name =  city[:name].to_s.strip.downcase.gsub(/\s/, '+')
-      url = "#{BASE_URL}weather?q=#{city_name}#{APP_ID}"
       results = self.get(url)
-      return { error: error_msg + results["message"] } if results["cod"] != 200
-      return self.update_city city, results
+      return { error: error_msg + results["message"] } if results["message"]
+      return self.update_cities(city_or_cities, results) unless single
+      return self.update_city(city_or_cities, results) if single
     rescue Exception => e
       msg = error_msg + e.to_s
       Rails.logger.fatal msg
@@ -30,15 +34,34 @@ class WeatherSearch
 
   end
 
-  def self.search_multiple_cities cities
+  def self.search_single_city city
+    city_name =  city[:name].to_s.strip.downcase.gsub(/\s/, '+')
+    url = "#{BASE_URL}weather?q=#{city_name}#{APP_ID}"
+    self.make_request url, city, "single"
+  end
 
+  def self.search_multiple_cities cities
+    ids = cities.map{|c| c[:external_id]}.join(",")
+    url = "#{BASE_URL}group?id=#{ids}#{APP_ID}"
+    self.make_request url, cities
   end
 
   def self.mock_single
-    city = {name: "Los Angeles",min: 70, max: 300,current: nil, in_range: false}
+    city = { name: "Los Angeles", min: 70, max: 300, current: "",
+            in_range: false, external_id: 5368361 }
     results = search_single_city city
     binding.pry
 
   end
+  def self.mock_multiple
+    city_one = { name: "Los Angeles", min: 70, max: 300, current: "",
+            in_range: false, external_id: 5368361 }
+    city_two = { name: "Vancouver", min: 70, max: 300, current: "",
+            in_range: false, external_id: 6173331 }
+    results = search_multiple_cities [city_one, city_two]
+    binding.pry
+
+  end
+
 end
 
